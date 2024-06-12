35806c51b1c1751a7426f1abb0e5c6c7f9769f4a
Thiago Soares <thiago_soares_mago@hotmail.com>
2024-06-12 12:42:49 -0300

    Fazendo deploy

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-aws^svhn9we*2lx+odnk$sy5d^$u+ff1^j91jutid-y#ho%_c=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'Papudim.pythonanywhere.com']
 
 
 # Application definition
